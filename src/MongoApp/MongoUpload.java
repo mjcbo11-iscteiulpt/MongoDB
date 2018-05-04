999c9f2ba5430c47d4d8f539acdc61112fb35f0e
Martim Machado <mttmo@iscte-iul.pt>
2018-05-04 17:26:44 +0100

    olo

--- src/MongoApp/MongoUpload.java
+++ src/MongoApp/MongoUpload.java
@@ -1,21 +1,27 @@
 package MongoApp;
 
+import java.util.LinkedList;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class MongoUpload extends Mongo implements Runnable{
 
-	
+	private LinkedList<MqttMessage> messages = new LinkedList<MqttMessage>();
+	private Paho p;
 	
 	public MongoUpload(Paho p) {
-		
+		this.p = p;
 	}
 	
-	protected void messageUp() {
-		
+	protected void messageUp(MqttMessage msg) {
+		for(MqttMessage m: messages) {
+			// Envia mongo!
+		}
 	}
 
 	@Override
 	public void run() {
 		while(true) {
-			
+			messages.add(p.retrieveMsg());
 		}
 		
 	}
