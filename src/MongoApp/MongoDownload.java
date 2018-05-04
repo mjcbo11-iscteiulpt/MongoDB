bd59c202248824d7c5ccb3d5615d3df4d27650e2
Bienti <Bienti@172.17.5.146>
2018-05-04 17:43:40 +0100

    ola2

--- src/MongoApp/MongoDownload.java
+++ src/MongoApp/MongoDownload.java
@@ -1,8 +1,26 @@
 package MongoApp;
 
-public class MongoDownload extends Mongo{
+public class MongoDownload extends Mongo implements Runnable{
 	
+	private int periodicidade = 60; //segundos
 	
+	public MongoDownload() {
+		
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+				//collection.
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
 	
 	
 }
