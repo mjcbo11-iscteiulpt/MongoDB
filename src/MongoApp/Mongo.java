8c40273d4968734b623a1c4fc4b85ed16087ca35
Bienti <Bienti@172.17.5.146>
2018-05-05 20:48:03 +0100

    Mongo Download
    MongoDownload já detecta dados com estado a zero e coloca a 1, a thread
    vai correndo de 10 em 10 segundos, foi criado outro main na classe Mongo
    para testar

--- src/MongoApp/Mongo.java
+++ src/MongoApp/Mongo.java
@@ -1,35 +1,44 @@
 package MongoApp;
 
+import org.bson.Document;
+
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
 
 	
 public class Mongo {
 	
 	private MongoClient mongoClient = null;
-	private DB database = null;
-	private DBCollection collection = null;
+	private MongoDatabase database = null;
+	private MongoCollection<Document> collection;
 	
-
+	public static void main(String[] args) {
+		(new Thread(new MongoDownload())).start();
+	}
 	public Mongo () {
 		mongoClient = new MongoClient();
-		database = mongoClient.getDB("LabMDB");
-		System.out.println("Connection Successful");
-		
+		database = mongoClient.getDatabase("LabMDB");
+		System.out.println("Connection Successful");		
 		collection = database.getCollection("HumidadeTemperatura");
-		System.out.println(database.getCollectionNames());
+		for(String nome: database.listCollectionNames()) {
+			System.out.println(nome);
+		}
 	}
 	
 	public MongoClient getMongoClient() {
 		return mongoClient;
 	}
 
-	public DB getDatabase() {
+	public MongoDatabase getDatabase() {
 		return database;
 	}
 
-	public DBCollection getCollection() {
+	public MongoCollection<Document> getCollection() {
 		return collection;
 	}
 	
--- src/MongoApp/MongoDownload.java
+++ src/MongoApp/MongoDownload.java
@@ -1,19 +1,71 @@
 package MongoApp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
+import static com.mongodb.client.model.Filters.*;
+
 public class MongoDownload extends Mongo implements Runnable{
 	
-	private int periodicidade = 60; //segundos
+	private int periodicidade = 10000; //milisegundos
+	private List<Integer> temperatura = new ArrayList<Integer>();
+	private List<Integer> humidade = new ArrayList<Integer>();
+	private List<String> data = new ArrayList<String>();
+	private List<String> hora = new ArrayList<String>();
+	FindIterable<Document> coll;
+	
 	
 	public MongoDownload() {
+		/*BasicDBObject query = new BasicDBObject();
+		BasicDBObject field = new BasicDBObject();
+		field.put("HumidadeTemperatura", 1);
+		DBCursor cursor = getCollection().find(query,field);
+		while (cursor.hasNext()) {
+		    BasicDBObject obj = (BasicDBObject) cursor.next();
+		    if(obj.getInt("Estado")!=1) {
+		    temperatura.add(obj.getInt("Temperatura"));
+		    humidade.add(obj.getInt("Humidade"));
+		    data.add(obj.getString("Data"));
+		    hora.add(obj.getString("Hora"));
+		    }
+		}*/
 		
+	}
+	
+	public void getNewValues() {
+		coll = getDatabase().getCollection("HumidadeTemperatura").find(eq("Estado", 0));
+		for(Document doc:coll) {
+			System.out.println("A temperatura é "+doc.getDouble("Temperatura")+" e a Humidade é "+doc.getDouble("Humidade"));
+			
+			//Aqui o valor vai ser enviado para o Sybase
+			
+			Bson filter = Filters.eq("_id", doc.getObjectId("_id"));
+		    Bson updates = Updates.set("Estado", 1);
+
+		    getCollection().findOneAndUpdate(filter, updates);
+		}
 	}
 
 	@Override
 	public void run() {
 		while(true) {
 			try {
-				Thread.sleep(1000);
-				//collection.
+				System.out.println("\n***************** \nComecar espera");
+				Thread.sleep(periodicidade);
+				getNewValues();
+				System.out.println("Valores alterados\n*****************\n");
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
